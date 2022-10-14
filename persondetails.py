3d540f80c25987b4bc8148dce5eb89c00c32cbdb
ch srihari <113081766+CHShari@users.noreply.github.com>
2022-10-14 09:01:32 +0530

    Create persondetails.py

---
+++ persondetails.py
@@ -0,0 +1,12 @@
+class Student_Details:
+    def __init__(self):
+        self.name = input("Enter Your Name: ")
+        self.cname = input("Enter Your College Name: ")
+        self.roll = int(input("Enter Your Roll Number: "))
+class Temp(Student_Details):
+    def display(self):
+        print("Name: ", self.name)
+        print("College Name:", self.cname)
+        print("Roll number:", self.roll)
+obj1 = Temp()
+obj1.display()
